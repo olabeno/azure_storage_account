65ba200a885dffb365563d698737fe78c145d154
Bernard Olatunde Oddiah <bernard.oddiah@gmail.com>
2025-05-12 13:04:46 +0100

    Update main.tf - 11

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "TFAzureLab-RG" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "olabeno12052025"
+  name                     = "olabeno1"
   resource_group_name      = azurerm_resource_group.TFAzureLab-RG.name
   location                 = azurerm_resource_group.TFAzureLab-RG.location
   account_tier             = "Standard"
@@ -18,7 +18,7 @@ resource "azurerm_storage_account" "storage" {
 
 resource "azurerm_storage_container" "container" {
   name                  = "mydatacontainer2025-05-12-xyz"
-  storage_account_id  = azurerm_storage_account.storage.name
+  storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
 ####
