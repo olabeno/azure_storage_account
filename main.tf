34c5bf0a063ec93a2127955382a2fd644dd381f9
Bernard Olatunde Oddiah <bernard.oddiah@gmail.com>
2025-05-12 11:52:11 +0100

    Update main.tf - 9

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_storage_account" "storage" {
 
 resource "azurerm_storage_container" "container" {
   name                  = "mydatacontainer2025-05-12-xyz"
-  storage_account_name  = azurerm_storage_account.storage.name
+  storage_account_id  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
 ###
